aef0cad6a2fcd4c5168b8ddd4fefdd736065079a
Artuur Everaert <artuur.everaert@student.hogent.be>
2020-10-04 22:00:55 +0200

    Add layout to recipepage

--- pages/recipe/[rid].js
+++ pages/recipe/[rid].js
@@ -1,16 +1,24 @@
-import { request } from '../../lib/datocms'
-import styles from '../../styles/recipe.module.scss'
+import { request } from "../../lib/datocms";
+import styles from "../../styles/recipe.module.scss";
+import Head from "next/Head";
+import { Image } from "react-datocms";
+
 
 const RECIPE_QUERY = `
 query MyQuery($id:ItemId) {
   recipe(filter: {id: {eq: $id}}) {
-    methode
+    
     tags
     title
     servers
     skilllevel
     id
     cookingtimeminutes
+    methodesteps {
+      id
+      steptext
+      istitle
+    }
     bannerimage {
       responsiveImage(imgixParams: { fit: crop, w: 300, h: 300, auto: format }) {
         srcSet
@@ -31,6 +39,7 @@ query MyQuery($id:ItemId) {
       unite
     }
     subimages {
+      id
      responsiveImage(imgixParams: { fit: crop, w: 300, h: 300, auto: format }) {
         srcSet
         webpSrcSet
@@ -48,28 +57,114 @@ query MyQuery($id:ItemId) {
 }
 `;
 
-export async function getServerSideProps({params }) {
-  
- 
-   const data = await request({
-    query: RECIPE_QUERY, 
-    variables: { id: params.rid }
-  })
-    return {  
-    props: {  
-      data  
-    },  
-   };  
-  } 
+export async function getServerSideProps({ params }) {
+  const data = await request({
+    query: RECIPE_QUERY,
+    variables: { id: params.rid },
+  });
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
-  const Recipe = (prop) => {
-    
-    
-  return <div>
-    <h1>
-      
-    </h1>
-  </div>
-  }
+const Recipe = (prop) => {
+  const rec = prop.data.recipe;
+  const hour = Math.floor(rec.cookingtimeminutes / 60);
+  const min = rec.cookingtimeminutes % 60;
+  var tags =  rec.tags.split(', ');
 
-export default Recipe
+  return (
+    <div>
+      <Head>
+        <title>ITP recipe {rec.title} </title>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+      </Head>
+      <div className={styles.recipepage}>
+        <div className="container">
+          <div className={styles.header}>
+            <div className="row align-items-center">
+              <div className="col-sm-5">
+                <Image
+                  data={rec.bannerimage.responsiveImage}
+                  className={styles.imageCard}
+                />
+              </div>
+              <div className="col-lg">
+                <h1>{rec.title}</h1>
+                <h2>aantal personen {rec.servers}</h2>
+               
+                <h2>
+                  {hour > 0 ? `${hour} uur en` : ""} {min} minuten{" "}
+                </h2>
+                
+                <div className={styles.tags}>
+                <div className= "row justify-content-center">{
+                    tags.map((tag) => (
+                      <div key={tag}  className="col-xs">
+                        <p className={styles.tag}>{tag}</p>
+                      </div>
+                       
+                    ))             
+                  }</div>
+                  </div>  
+                
+               
+              </div>
+            </div>
+          </div>
+          <div className={styles.recipeBody}>
+            <div className="row">
+              <div className="col-auto">
+                <div className={styles.ingredientsList}>
+                  <h2>Ingredienten</h2>
+                  {rec.ingredients.map((ing) => (
+                    <p key={ing.name} >
+                      {ing.amount} {ing.unite} {ing.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div className="col-md">
+                <h2>Bereiding</h2>
+                {
+                  rec.methodesteps.map((step) => (
+                  <div key={step.id} >
+                    {step.istitle != true ? <p>{step.steptext}  </p> : <p className={styles.stepSubTitle}>{step.steptext} </p>}
+                  </div>
+
+                    )
+                  )
+                }
+              </div>
+            </div>
+            <div className={styles.subimages}>
+              <div className="row justify-content-md-center ">
+                {rec.subimages.map((subimage) => (
+                  <div key={subimage.id} className="col-auto">
+                    <Image
+                      data={subimage.responsiveImage}
+                      className={styles.subImageCard}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
